b38fda2654e67d941feac95e1af934c6437129e0
Harshsarang <harshsarang0@gmail.com>
2025-10-19 15:20:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,54 +3,59 @@ import pickle
 import pandas as pd
 import requests
 
-def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/ {}?api_key=4e0e1d79d4b3ad7998770663f336b2ba&language=en-US'.format(movie_id))
-    data = response.json()
-    return "http://image.tmdb.org/t/p/w500" + data['poster_path']
-
-
-def recommend(movie):
-    movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-
-    recommended_movies = []
-    recommended_movies_poster = []
-    for i in movies_list:
-        movie_id = movies.iloc[i[0]].id
-        recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_movies_poster.append(fetch_poster(movie_id))
-    return recommended_movies, recommended_movies_poster
-
-movies_dict = pickle.loads(open('movies_dict.pkl', 'rb').read())
+# --- Load your saved data ---
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
-similarity = pickle.loads(open('similarity.pkl', 'rb').read())
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-st.title('Movie Recommendation System')
+# --- Function to fetch poster ---
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=4e0e1d79d4b3ad7998770663f336b2ba&language=en-US"
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        return "https://image.tmdb.org/t/p/w500" + data['poster_path']
+    except Exception as e:
+        print(f"Error fetching poster for movie_id {movie_id}: {e}")
+        return None
 
-selected_movies_name = st.selectbox(
-    'How you like to be contacted?',
+# --- Function to recommend top 5 similar movies ---
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = similarity[index]
+    movies_list = sorted(
+        list(enumerate(distances)), reverse=True, key=lambda x: x[1]
+    )[1:6]  # top 5 movies (skip the first one because it's the same movie)
+
+    recommended_movies = []
+    recommended_posters = []
+
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]]['id']
+        recommended_movies.append(movies.iloc[i[0]].title)
+        recommended_posters.append(fetch_poster(movie_id))
+
+    return recommended_movies, recommended_posters
+
+# --- Streamlit app ---
+st.title('🎬 Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+    'Select a movie to get recommendations:',
     movies['title'].values
 )
 
-if st.button('Get Movie Recommendations'):
-    names, posters = recommend(selected_movies_name)
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
 
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.header(names[0])
-        st.image(posters[0])
-    with col2:
-        st.header(names[1])
-        st.image(posters[1])
-    with col3:
-        st.header(names[2])
-        st.image(posters[2])
-    with col4:
-        st.header(names[3])
-        st.image(posters[3])
-    with col5:
-        st.header(names[4])
-        st.image(posters[4])
-
+    # Display 5 recommendations side by side
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.text(names[i])
+            if posters[i]:
+                st.image(posters[i])
+            else:
+                st.write("Poster unavailable")
